4d2d60b254584ee150f2be75356bc53bbb3691cb
Chris Hill-Scott <me@quis.cc>
2017-02-16 13:06:53 +0000

    Add Google Maps API key

--- source/themes/quis/scripts.php
+++ source/themes/quis/scripts.php
@@ -1,5 +1,5 @@
 <script src="//code.jquery.com/jquery-2.1.3.min.js"></script>
-<script src="//maps.googleapis.com/maps/api/js?v=3.exp&amp;sensor=false&amp;libraries=geometry"></script>
+<script src="//maps.googleapis.com/maps/api/js?v=3.exp&amp;key=AIzaSyDtkPdwkkhmFXLNp9Z4k8JnaLh_yXmSzQ4&amp;libraries=geometry"></script>
 <script src="/wp-content/themes/quis/js/quis.js"></script>
 
 <script>
