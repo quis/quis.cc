9c6d8f9d93d22751a6e95496229d6c9befc77ba0
Chris Hill-Scott <me@quis.cc>
2024-12-27 13:34:19 +0000

    Show date archives in chronological order

--- source/themes/quis/index.php
+++ source/themes/quis/index.php
@@ -5,6 +5,7 @@
   global $query_string;
   $query_modifications = is_category() ? "" : "&cat=-7"; // Exclude hidden posts unless on a category page
   $query_modifications .= parent_category_is("trips") ? "&order=ASC" : "";
+  $query_modifications .= is_date() ? "&order=ASC" : "";
   query_posts($query_string.$query_modifications);
 
   is_front_page() && !is_paged() ? require("nav.php") : null;
